0d32610883f51ac0b12a3352bf26984934d14215
Nestor Muñoz <nesfabian2019@gmail.com>
2026-02-13 22:27:23 -0600

    Configuracion 13-02-2026

---
+++ config.py
@@ -0,0 +1,13 @@
+import os 
+
+from  sqlalchemy import create_engine
+
+class Config(object):
+    SECRET_KEY ="ClaveSecreta"
+    SESSION_COOKIE_SEGURE=False
+    
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://nesfa:210804@127.0.0.1/bdidgs805'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
